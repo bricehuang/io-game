ed2e24c915c7520fe3251db55e248d03ea64e611
Brice Huang <bmhuang@mit.edu>
2018-01-14 22:16:10 -0500

    temp

--- client/js/game.js
+++ client/js/game.js
@@ -25,9 +25,9 @@ Game.prototype.handleGraphics = function(gfx) {
 
   gfx.fillStyle = '#2ecc71';
   gfx.strokeStyle = '#27ae60';
-  gfx.font = 'bold 50px Verdana';
+  gfx.font = '12px Verdana';
   gfx.textAlign = 'center';
   gfx.lineWidth = 2;
-  gfx.fillText('Now playing...', screenWidth / 2, screenHeight / 2);
-  gfx.strokeText('Now playing...', screenWidth / 2, screenHeight / 2);
+  gfx.fillText(playerName, screenWidth / 2, screenHeight / 2);
+  // gfx.strokeText('Now playing...', screenWidth / 2, screenHeight / 2);
 }
