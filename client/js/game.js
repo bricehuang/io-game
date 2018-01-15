cd23351a27115e05cf72d7f37a3d5a721d8b57c7
Brice Huang <bmhuang@mit.edu>
2018-01-14 22:40:23 -0500

    Map drawing

--- client/js/game.js
+++ client/js/game.js
@@ -1,5 +1,7 @@
 function Game() { };
 
+var nearby_objects = [];
+
 Game.prototype.handleNetwork = function(socket) {
   console.log('Game connection process here');
   console.log(socket);
@@ -9,6 +11,9 @@ Game.prototype.handleNetwork = function(socket) {
 
   socket.on('bearing', function(message){
     console.log('bearing: ' + message);
+  })
+  socket.on('game_state', function(message){
+    nearby_objects = message
   })
   // This is where you receive all socket messages
 }
@@ -24,10 +29,18 @@ Game.prototype.handleGraphics = function(gfx) {
   gfx.fillRect(0, 0, screenWidth, screenHeight);
 
   gfx.fillStyle = '#2ecc71';
-  gfx.strokeStyle = '#27ae60';
+  gfx.strokeStyle = '#003300';
   gfx.font = '12px Verdana';
   gfx.textAlign = 'center';
-  gfx.lineWidth = 2;
-  gfx.fillText(playerName, screenWidth / 2, screenHeight / 2);
+  for (var i=0; i<nearby_objects.length; i++) {
+    var object = nearby_objects[i];
+    var centerX = screenWidth/2 + object.x;
+    var centerY = screenHeight/2 + object.y;
+    var radius = 30;
+    gfx.fillText(object.name, centerX, centerY+4);
+    gfx.beginPath();
+    gfx.arc(centerX, centerY, radius, 0, 2*Math.PI, false);
+    gfx.stroke();
+  }
   // gfx.strokeText('Now playing...', screenWidth / 2, screenHeight / 2);
 }
