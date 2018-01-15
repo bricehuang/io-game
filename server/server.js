8859c22015976be1f93b6e239af363fd6a61fff4
Brice Huang <bmhuang@mit.edu>
2018-01-14 21:53:18 -0500

    Some protocols

--- server/server.js
+++ server/server.js
@@ -20,3 +20,41 @@ var serverPort = process.env.PORT || config.port;
 http.listen(serverPort, function() {
   console.log("Server is listening on port " + serverPort);
 });
+
+/*
+Players should have fields:
+player.name (string, name)
+player.y (float, absolute x-coordinate in game grid)
+player.y (float, absolute y-coordinate in game grid)
+player.socket (player's socket)
+player.windowWidth (width of player's client window)
+player.windowHeight (height of player's client window)
+*/
+
+/*
+Return an array
+  [
+    {
+      name: __
+      x: __
+      y: __
+    },
+    {
+      name: __
+      x: __
+      y: __
+    },
+    ...
+  ]
+where each element in this array is a player within
+x-distance player.windowWidth/2 and y-distance
+player.windowHeight/2 from this player.  For each object,
+the x,y values returned represent the location of the
+player relative to this player.
+*/
+function sendView(player) {
+  // TODO replace "" with something
+  player.socket.emit('game_state', "")
+}
+
+
