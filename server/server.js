c87765616a1a90ef7c73517f33f7425767ca85ca
Michael <mkural@mit.edu>
2018-01-14 22:42:06 -0500

    add movement and player list

--- server/server.js
+++ server/server.js
@@ -4,18 +4,60 @@ var http    = require('http').Server(app);
 var io      = require('socket.io')(http);
 
 var config  = require('./config.json');
+var players = [];
 
 app.use(express.static(__dirname + '/../client'));
 
 io.on('connection', function (socket) {
   console.log("Somebody connected!");
   // Write your code here
+  nextId = players.length;
+  currentPlayer = {
+    name:config.defaultName,
+    x:0,
+    y:0,
+    socket:socket,
+    windowHeight : config.defaultWindowHeight,
+    windowWidth  : config.defaultWindowWidth;
+    id : nextId;
+    target  : {x:0,y:0}
+  }
+  players.push(currentPlayer);
+
+  socket.on('player_information', function(data){
+    currentPlayer.name         = data.playerName;
+    currentPlayer.windowWidth  = data.playerWidth;
+    currentPlayer.windowHeight = data.playerHeight;
+  });
+
   socket.on('mouse_location', function(message){
+    currentPlayer.target = {x:message.x,y:message.y};
     var bearing = Math.atan2(message.x, message.y) * 180 / Math.PI;
     socket.emit('bearing', bearing);
-  })
-});
+  });
 
+});
+function movePlayer(player){
+  var x = player.target.x;
+  var y = player.target.y;
+  var maxSpeed = 6;
+  var speed;
+  var dist = Math.sqrt(x*x+y*y);
+  if(dist>50){
+    speed = maxSpeed;
+  }
+  else if(dist<20){
+    speed = 0;
+  }
+  else{
+    speed = (dist-20)* (maxSpeed)/30;
+  }
+  var deg = Math.atan2(x,y);
+  var changeX = speed*cos(deg);
+  var changeY = speed*sin(deg);
+  player.x +=changeX;
+  player.y +=changeY;
+}
 var serverPort = process.env.PORT || config.port;
 http.listen(serverPort, function() {
   console.log("Server is listening on port " + serverPort);
