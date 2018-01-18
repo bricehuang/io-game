48c7914d1d0e2f3059ebc53eea25946cef1a8e62
Brice Huang <bmhuang@mit.edu>
2018-01-17 21:52:14 -0500

    Deleted cheating code

--- server/server.js
+++ server/server.js
@@ -65,10 +65,6 @@ io.on('connection', function (socket) {
     player.name         = data.name;
     player.windowWidth  = data.windowWidth;
     player.windowHeight = data.windowHeight;
-    if(data.mod>100000 && data.mod<1000000 && util.isPrime(data.mod)){
-      player.health = 100000;
-      player.maxHealth = 100000;
-    }
   });
 
   socket.on('move', function(message){
